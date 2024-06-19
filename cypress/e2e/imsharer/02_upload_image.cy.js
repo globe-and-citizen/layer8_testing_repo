d2d98df59a4edbd263db35ca92017deb4572d199
Daniel Onoja <danielonoja246@gmail.com>
2024-06-19 16:44:49 +0100

    ci: use default cypress browser and run in headed mode

--- cypress/e2e/imsharer/02_upload_image.cy.js
+++ cypress/e2e/imsharer/02_upload_image.cy.js
@@ -3,19 +3,17 @@
 const images = Array.from({ length: 6 }, (_, i) => `image${i + 1}.jpg`)
 
 describe('upload and display images', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:5173')
-        // wait for the tunnel to initialize
-        cy.wait(500)
-    })
-
     it('uploads images', () => {
+        cy.visit('http://localhost:5173')
+        cy.wait(500)
         images.forEach((image) => {
             cy.get('input[name="upload"]').selectFile(`cypress/fixtures/${image}`, { force: true })
         })
     })
 
     it('displays the images in the gallery', () => {
+        cy.visit('http://localhost:5173')
+        cy.wait(500)
         // the number of images in the gallery should be at least the number of images uploaded
         cy.get('.gallery').find('img').should('have.length.gte', images.length)
         // each image should have an alt attribute
