631b276b1a15c169657be095c067fd68181af7b8
Daniel Onoja <danielonoja246@gmail.com>
2024-06-19 17:45:28 +0100

    feat: rewrite upload image spec

--- cypress/e2e/imsharer/02_upload_image.cy.js
+++ cypress/e2e/imsharer/02_upload_image.cy.js
@@ -1,24 +1,36 @@
 /// <reference types="cypress" />
 
-const images = Array.from({ length: 6 }, (_, i) => `image${i + 1}.jpg`)
-
 describe('upload and display images', () => {
-    it('uploads images', () => {
+    it('uploads images and confirms they are displayed', () => {
         cy.visit('http://localhost:5173')
         cy.wait(500)
-        images.forEach((image) => {
-            cy.get('input[name="upload"]').selectFile(`cypress/fixtures/${image}`, { force: true })
-        })
-    })
 
-    it('displays the images in the gallery', () => {
-        cy.visit('http://localhost:5173')
+        // upload images
+        cy.get('input[name="upload"]').selectFile('cypress/fixtures/image1.jpg', { force: true })
         cy.wait(500)
+        cy.get('img[alt="image1.jpg"]').should('exist')
+
+        cy.get('input[name="upload"]').selectFile('cypress/fixtures/image2.jpg', { force: true })
+        cy.wait(500)
+        cy.get('img[alt="image2.jpg"]').should('exist')
+
+        cy.get('input[name="upload"]').selectFile('cypress/fixtures/image3.jpg', { force: true })
+        cy.wait(500)
+        cy.get('img[alt="image3.jpg"]').should('exist')
+
+        cy.get('input[name="upload"]').selectFile('cypress/fixtures/image4.jpg', { force: true })
+        cy.wait(500)
+        cy.get('img[alt="image4.jpg"]').should('exist')
+
+        cy.get('input[name="upload"]').selectFile('cypress/fixtures/image5.jpg', { force: true })
+        cy.wait(500)
+        cy.get('img[alt="image5.jpg"]').should('exist')
+
+        cy.get('input[name="upload"]').selectFile('cypress/fixtures/image6.jpg', { force: true })
+        cy.wait(500)
+        cy.get('img[alt="image6.jpg"]').should('exist')
+
         // the number of images in the gallery should be at least the number of images uploaded
-        cy.get('.gallery').find('img').should('have.length.gte', images.length)
-        // each image should have an alt attribute
-        images.forEach((image) => {
-            cy.get(`img[alt="${image}"]`).should('exist')
-        })
+        cy.get('.gallery').find('img').should('have.length.gte', 6)
     })
 })
