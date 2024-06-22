b3b2633d565ab79f159a09b413f47d9fcd1fdef4
Daniel Onoja <danielonoja246@gmail.com>
2024-06-22 13:13:34 +0100

    chore: remove uneccessary waits

--- cypress/e2e/imsharer/02_upload_image.cy.js
+++ cypress/e2e/imsharer/02_upload_image.cy.js
@@ -7,27 +7,21 @@ describe('upload and display images', () => {
 
         // upload images
         cy.get('input[name="upload"]').selectFile('cypress/fixtures/image1.jpg', { force: true })
-        cy.wait(500)
         cy.get('img[alt="image1.jpg"]').should('exist')
 
         cy.get('input[name="upload"]').selectFile('cypress/fixtures/image2.jpg', { force: true })
-        cy.wait(500)
         cy.get('img[alt="image2.jpg"]').should('exist')
 
         cy.get('input[name="upload"]').selectFile('cypress/fixtures/image3.jpg', { force: true })
-        cy.wait(500)
         cy.get('img[alt="image3.jpg"]').should('exist')
 
         cy.get('input[name="upload"]').selectFile('cypress/fixtures/image4.jpg', { force: true })
-        cy.wait(500)
         cy.get('img[alt="image4.jpg"]').should('exist')
 
         cy.get('input[name="upload"]').selectFile('cypress/fixtures/image5.jpg', { force: true })
-        cy.wait(500)
         cy.get('img[alt="image5.jpg"]').should('exist')
 
         cy.get('input[name="upload"]').selectFile('cypress/fixtures/image6.jpg', { force: true })
-        cy.wait(500)
         cy.get('img[alt="image6.jpg"]').should('exist')
 
         // the number of images in the gallery should be at least the number of images uploaded
